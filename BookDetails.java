0c8d582cdb946c06c529945dd095c899b5bf1aa9
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2025-08-06 15:26:25 +0530

    Add files via upload

--- BookDetails.java
+++ BookDetails.java
@@ -1,5 +1,43 @@
 package j_lab;
+class Book{
+	String title;
+	String author;
+	final int bookID;
+	static int bookCounter = 1000;
+	final static String LIBRARY_NAME = "Central Library";
+	Book(){
+		title = "No title";
+		author = "No author";
+		bookID = bookCounter;
+		bookCounter++;
+	}
+	Book(String title,String author){
+		this.title = title;
+		this.author = author;
+		bookID = bookCounter;
+		bookCounter++;
+	}
+	public void displayInfo() {
+		System.out.println("Title:"+title);
+		System.out.println("Author:"+author);
+		System.out.println("Book ID:"+bookID);
+	}
+	public void displayInfo(boolean showLibrary) {
+		if(showLibrary) {
+			displayInfo();
+			System.out.println("Library Name:"+LIBRARY_NAME);
+		}
+	}
+	public void displayTotalBooks() {
+		System.out.println("Total Books:"+(bookCounter-1000));
+	}
+}
 
 public class BookDetails {
+	public static void main(String []args) {
+	Book book = new Book();
+	book.displayInfo(true);
+	book.displayTotalBooks();
 
+}
 }
